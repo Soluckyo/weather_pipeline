988bcbb23cd02e02e41a3c0dafacc2e43dd8204c
Soluckyo <twinki0000@yandex.ru>
2026-03-18 18:11:55 +0300

    Поправил логи, добавил порт для подключения

--- logger.py
+++ logger.py
@@ -2,16 +2,14 @@ import logging
 
 def get_logger(name: str):
     logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
 
-    if not logger.handlers:
-        logger.setLevel(logging.INFO)
 
+    if not logger.hasHandlers():
         handler = logging.StreamHandler()
-
         formatter = logging.Formatter(
             "%(asctime)s | %(levelname)s | %(message)s"
-            )
-        
+            )       
         handler.setFormatter(formatter)
         logger.addHandler(handler)
 
--- validation.py
+++ validation.py
@@ -1,14 +1,22 @@
+from logger import get_logger
 
+logger = get_logger(__name__)
 
 def is_valid_record(record):
     if record.temperature is None:
+        logger.warning(f"Invalid temp (null): {record}")
         return False
     
     if record.temperature < -100 or record.temperature > 60:
+        logger.warning(f"Invalid temp: {record}")
         return False
     
     if record.wind_speed < 0 or record.wind_speed > 150:
+        logger.warning(f"Invalid wind: {record}")
         return False
     
     if record.precipitation < 0:
+        logger.warning(f"Invalid precip: {record}")
         return False
+    
+    return True
